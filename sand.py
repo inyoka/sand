0c78abad7fde34265ce6295641ee4fdd22ac4553
inyoka <simonmarkholland@gmail.com>
2016-11-15 14:16:46 +0700

    Modified logic, produces simple list of answer.

--- sand.py
+++ sand.py
@@ -1,58 +1,51 @@
+#!/usr/bin/env python3.5 -tt
 import tkinter as tk 
-from tkinter import ttk
 
 file = open('questions.txt')
-questionlist = file.readlines()
+questions = file.readlines()
 
+class Form(tk.Frame):
+    def __init__(self, parent, questions):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
 
-class GUI(ttk.Frame):
-    def __init__(self, master, questionlist):
-        ttk.Frame.__init__(self, master)
-
-        title = ttk.Label(self, text="SURVEY")
+        title = tk.Label(self, text="Strengths & Difficulties")
         title.pack()
 
-        self.questions = Questions(self, questionlist)
+        self.questions = Questions(self, questions)
         self.questions.pack()
 
-        done_button = ttk.Button(self, text="Done.", command=self.done)
+        done_button = tk.Button(self, text="Done.", command=self.done)
         done_button.pack()
 
     def done(self):
-        for q, a in self.questions.q_and_a:
-            print("{}: {}".format(q,a.get()))
-        quit()
+        for answer in self.questions.answers:
+            print(answer.get(), end=" ")
+            #if answer.get()  is -1 
+        print('')    
+        root.quit()
 
-class Questions(ttk.Frame):
-    '''I put all the questions into a single class so they would line up better'''
-    def __init__(self, master, questionlist):
-        ttk.Frame.__init__(self, master)
-        '''
-        self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")
-        self.frame = tk.Frame(self.canvas, background="#ffffff")
-        self.vsb = tk.Scrollbar(self.canvas, orient="vertical", command=self.canvas.yview)
-        self.canvas.configure(yscrollcommand=self.vsb.set)
-        self.vsb.pack(side="right", fill="y")
-        self.canvas.pack(side="left", fill="both", expand=True)
-        #self.canvas.create_window((4,4), window=self.frame, anchor="nw", tags="self.frame")
-        #self.frame.bind("<Configure>", self.onFrameConfigure)
-        '''
-        self.populate()
+class Questions(tk.Frame):
+    def __init__(self, master, questions):
+        tk.Frame.__init__(self, master)
+        self.askQuestions()
 
+    def askQuestions(self):
+        self.answers = []
+        for count, question in enumerate(questions):
+            var = tk.IntVar(value = -1)
+            number = count+1
+            width = 5
+            line = '{:5}'.format(count+1, fill=' ') + ' : ' + question.strip()
+            label = tk.Label(self, text=line)
+            label.grid(row=count, column = 0, sticky=tk.W)
+            for answer in range(0,4):
+                button = tk.Radiobutton(self, variable = var, value = answer)
+                button.grid(row = count, column = answer+1)
+            self.answers.append((var))
 
-    def populate(self):
-        self.q_and_a = []
-        for row, question in enumerate(questionlist):
-            var = tk.IntVar(value = -1) #-1 is sorta standard for "not answered", so it's a good default value
-            q_label = ttk.Label(self, text=question)
-            q_label.grid(row=row, column = 0)
-            for i in range(0,3):
-                button = tk.Radiobutton(self, variable = var, value = i+1)
-                button.grid(row = row, column = i+1)
-            self.q_and_a.append((question, var))
-
-
-root = tk.Tk()
-window = GUI(root, questionlist)
-window.pack()
-root.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    window = Form(root, questions)
+    window.pack()
+    root.mainloop()
