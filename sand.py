5940ab0d0ff0d71a2318d6b7e763da9963307125
inyoka <simonmarkholland@gmail.com>
2016-11-25 17:04:42 +0700

    Added quit button, started on scrolled text output.

--- sand.py
+++ sand.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3 -tt
 import tkinter as tk
 from tkinter.filedialog import asksaveasfile
+from tkinter.messagebox import askquestion
+from tkinter import scrolledtext
 import dicttools
 import csv
 import sys
@@ -17,12 +19,8 @@ class MainApplication(tk.Frame):
         self.footer = Footer(self, information)
         self.header.pack(side='top', fill='x', expand=True)
         self.questionnaire.pack(side='top', fill='x', expand=True)
-        self.footer.pack(side='bottom', fill='x', expand=True) 
-
-    def exit(self):
-        # Stub awaiting an seperate quit button.
-        root.quit()
-
+        self.footer.pack(side='bottom', fill='x', expand=True)
+  
 class Info():
     def __init__(self):
         file = open('questions.txt')
@@ -32,11 +30,29 @@ class Info():
                         'hyperactivity' : [2, 10, 15, 21, 25], 
                         'peer' : [6, 11, 14, 19, 23], 
                         'prosocial' : [1, 4, 9, 17, 20]}
-        self.answers = []
+        self.buttons = []
         self.incomplete = set()
         self.name = tk.StringVar()
+        self.dob = tk.StringVar() 
+        self.answers = []
         self.finalScore = {}
-        self.dob = tk.StringVar()
+
+    def resetConfirm(self):
+        result = askquestion("Delete", "Are You Sure?", icon='warning')
+        if result == 'yes':
+            print ("Deleted")
+            self.resetFields()
+        else:
+            print ("I'm Not Deleted Yet")
+
+    def resetFields(self):
+        self.name.set(value = '')
+        self.dob.set(value = '')
+        #self.answers = []
+        #for _count in self.answers:
+        #    self.answers.append(tk.IntVar(value = -1))
+        for count, button in enumerate(self.buttons):
+            self.buttons[count] = tk.IntVar(value = -1)
 
 class Header(tk.Frame):
     def __init__(self, parent, information):
@@ -52,7 +68,7 @@ class Header(tk.Frame):
         nameEntry = tk.Entry(clientDetails, textvariable=self.info.name).grid(row=0, column=1)
         dobLabel = tk.Label(clientDetails, text='Date(MM/DD/YYYY)').grid(row=1, column=0)
         self.dobEntry = tk.Entry(clientDetails, textvariable=self.info.dob).grid(row=1, column=1)
-        dobButton = tk.Button(clientDetails, text='DoB', command=self.formatDateWidget).grid(row=1, column=2)
+        dobButton = tk.Button(clientDetails, text='Format DoB', command=self.formatDateWidget).grid(row=1, column=2)
         clientDetails.pack(fill='both', expand=True, side=tk.TOP)
 
         header = tk.Frame(self)
@@ -67,7 +83,7 @@ class Header(tk.Frame):
         for pos in [2, 5]:
             if len(entrylist) > pos:
                 entrylist.insert(pos, '/')
-        self.info.dob.set(' '.join(entrylist))
+        self.info.dob.set(''.join(entrylist))
 
 class Questionnaire(tk.Frame):
     def __init__(self, master, information):
@@ -80,7 +96,7 @@ class Questionnaire(tk.Frame):
         file = open('questions.txt')
         questionlist = file.readlines()
         for number, question in enumerate(questionlist, 1):
-            var = tk.IntVar(value = -1)
+            self.var = tk.IntVar(value = -1)
             width = 5
             line = '{:5}'.format(number, fill=' ') + ' : ' + question.strip()
             label = tk.Label(self, text=line)
@@ -89,18 +105,26 @@ class Questionnaire(tk.Frame):
             label.grid(row=number, column = 0, sticky=tk.W)
             options = ['?', 'No', 'Maybe', 'Yes']
             for answer in range(-1,3):
-                button = tk.Radiobutton(self, borderwidth=10, variable = var, text=options[answer+1], width = 5, value = answer, indicatoron=0)
+                button = tk.Radiobutton(self, borderwidth=10, variable = self.var, text=options[answer+1], width = 5, value = answer, indicatoron=0)
                 if number % 5 == 0:
                     button.configure(background='#d0d0d0')
                 button.grid(row = number, column = answer+2)
-            self.info.answers.append(var)
+                self.info.buttons.append(button)
+
+            self.info.answers.append(self.var)
 
 class Footer(tk.Frame):
     def __init__(self, parent, information):
         super(Footer, self).__init__()
+        self.parent = parent
         self.info = information
-        done_button = tk.Button(self, text="Done", command=self.createReport)
-        done_button.pack()
+        footer = tk.Frame(self)
+        reset_button = tk.Button(footer, text="Reset All", command=self.info.resetConfirm).grid(row=0, column=0, sticky='w')
+        footer.grid_columnconfigure(1, weight=1)
+        output_button = tk.Button(footer, text="Output", command=self.outputResults).grid(row=0, column=2)
+        done_button = tk.Button(footer, text="Done", command=self.createReport).grid(row=0, column=3)
+        exit_button = tk.Button(footer, text="Quit", command=self.exit).grid(row=0, column=4)
+        footer.pack(fill='both', expand=True, side=tk.TOP, pady=5)
 
     def convertToInts(self):
         self.info.answers = [ int(x.get()) for x in self.info.answers ]
@@ -109,6 +133,7 @@ class Footer(tk.Frame):
         results = []
         reverse = [7, 11, 14, 21, 25]
         for no, answer in enumerate(self.info.answers, 1):
+            # Seems like slices would be better suited to this
             if no in reverse:
                 if answer == 0:
                     answer = 2
@@ -131,38 +156,44 @@ class Footer(tk.Frame):
         for trait in self.info.traits.keys():
             self.info.finalScore[trait] = self.addScore(trait)
 
-    def printResults(self):
-        print('Name : ', self.info.name.get())
-        dicttools.dump(self.info.finalScore)
+    def outputResults(self):
+        top = tk.Tk()
+        #Toplevel()
+        top.title("Sample output ...")
+
+        messageScrolled = scrolledtext.ScrolledText(top, wrap=tk.WORD, width=20, height=10)
+        messageScrolled.pack(side='top', fill='x', expand=True)
+
+        messageScrolled.insert(tk.INSERT, self.info.name.get())
+        messageScrolled.insert(tk.INSERT, self.info.dob.get())
+        messageScrolled.insert(tk.INSERT, self.info.incomplete)
+
+
+        button = tk.Button(top, text="Dismiss", command=top.destroy)
+        button.pack()
+        '''
         print(self.info.incomplete)
         print(time.strftime("%a %d-%m-%Y %H:%M:%S", time.gmtime()))
-
+        print('Client name :', self.info.name.get())
+        print('Birth date  :', self.info.dob.get())
+        print('Survey date :', time.strftime("%a %d-%m-%Y %H:%M:%S", time.gmtime()))
+        print('Incomplete  :', self.info.incomplete)
+        dicttools.dump(self.info.finalScore)
+        '''
     def fileSave(self):
         name = asksaveasfile(mode='w', defaultextension=".csv") 
         if name is None:
             return
         with name as f:
-            w = csv.writer(f) #switch to file later
+            w = csv.writer(f)
             w.writerow(['Client name :']+[self.info.name.get()])
-            #w.writerow(self.info.age.get())
+            w.writerow(['Birth date  :']+[self.info.dob.get()])
             w.writerow(['Survey date :']+[time.strftime("%a %d-%m-%Y %H:%M:%S", time.gmtime())])
             w.writerow(['Incomplete  :']+[self.info.incomplete])
             w.writerows(self.info.finalScore.items())
             f.close()
 
-    def outputCSV(self):
-        # This is residual code unused at the moment.
-        filename = self.info.name.get() + time.strftime("-%d%m%Y-%H%M")+'.csv'
-        print('Filename : ', filename)
-        with open(filename,'wb') as f:
-            w = csv.writer(sys.stderr) #switch to file later
-            w.writerow(['Client name :']+[self.info.name.get()])
-            w.writerow(self.info.dob.get())
-            w.writerow(['Survey date :']+[time.strftime("%a %d-%m-%Y %H:%M:%S", time.gmtime())])
-            w.writerow(['Incomplete  :']+self.info.incomplete)
-            w.writerows(self.info.finalScore.items())
-
-    def diplayReport(self):
+    def displayReport(self):
         # Will eventual display the report results in a window. 
         self.convertToInts()
         self.reverseAnswers()
@@ -175,11 +206,11 @@ class Footer(tk.Frame):
         self.reverseAnswers()
         self.sumTraits()
         self.fileSave()
-        root.quit() # To be removed when quit button created.
 
-    def resetFields(self):
-        # Stub for resetting fields after a successful export.
-        pass
+    def exit(self):
+        # Stub awaiting an seperate quit button.
+        print('Exiting application')
+        root.quit()
 
 if __name__ == '__main__':
     root = tk.Tk()
