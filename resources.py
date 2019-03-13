390f40fd9cb2ea044c1b6c41de73a31de087c22a
Simon <simonmarkholland@gmail.com>
2019-03-13 14:08:47 +0700

    Create csv and paste window resources

--- resources.py
+++ resources.py
@@ -25,3 +25,49 @@ questions = [
     "Many fears, easily scared",
     "Sees tasks through to the end, good attention span"
 ]
+
+def textLines(self):
+    lines = [
+        ('Birth date  :' + self.dob.get() + '\n'),
+        ('Client name :' + self.name.get() + '\n'),
+        ('Survey date :' + self.time + '\n'),
+        ('Incomplete  :' + str(list(self.incomplete)) + '\n'),
+        ('Stress score:' + str(self.stressScore) + '\n'),
+        ('Emotional distress :' + str(self.fnlScore.get('emotional')) + '\n'),
+        ('Behavioural difficulties :' + str(self.fnlScore.get('conduct')) + '\n'),
+        ('Hyperactivity and concentration difficulties :' + str(self.fnlScore.get('hyperactivity')) + '\n'),
+        ('Difficulties socialising with children :' + str(self.fnlScore.get('peer')) + '\n'),
+        ('Kind and helpful behaviour :' + str(self.fnlScore.get('prosocial')) + '\n')
+    ]
+    return lines
+
+def csvLines(self):
+    lines = [
+        (['Client ID    :']+[self.name.get()]),
+        (['Birth date   :']+[self.dob.get()]),
+        (['Survey date  :']+[self.time]),
+        (['Incomplete   :']+[a for a in list(self.incomplete)]),
+        (['PRO-SOCIAL   :']+[str(self.fnlScore['prosocial'])]),
+        (['Hyperactivity:']+[str(self.fnlScore['hyperactivity'])]),
+        (['Emotional    :']+[str(self.fnlScore['emotional'])]),
+        (['Conduct      :']+[str(self.fnlScore['conduct'])]),
+        (['Peer         :']+[str(self.fnlScore['peer'])]),
+        (['Total score  :']+[self.stressScore])
+    ]
+    return lines
+
+
+def windowLines(self):
+    lines = [
+        ('Client name  :'+self.name.get()+'\n'),
+        ('Birth date   :'+self.dob.get()+'\n'),
+        ('Survey date  :'+self.date.get()+'\n'),
+        ('Incomplete   :'+', '.join(a for a in self.incomplete)+'\n'),
+        ('PRO-SOCIAL   :'+str(self.fnlScore['prosocial'])+'\n'),
+        ('Hyperactivity:'+str(self.fnlScore['hyperactivity'])+'\n'),
+        ('Emotional    :'+str(self.fnlScore['emotional'])+'\n'),
+        ('Conduct      :'+str(self.fnlScore['conduct'])+'\n'),
+        ('Peer         :'+str(self.fnlScore['peer'])+'\n'),
+        ('Total score  :'+str(self.stressScore))
+    ]
+    return lines
